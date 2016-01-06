e2365f2ae2dfd96996f8a17fdb4767901b790191
yalab <rudeboyjet@gmail.com>
2016-01-06 18:58:46 +0900

    It can import local postgres.

---
+++ create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE meteors(
+       stamp   VARCHAR(255),
+       status  VARCHAR(255),
+       url     VARCHAR(255),
+       address VARCHAR(255),
+       agent   VARCHAR(255),
+       referer VARCHAR(255),
+       scene   VARCHAR(255),
+       time    VARCHAR(255)
+);
--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var aws = require('aws-sdk');
 var s3 = new aws.S3({apiVersion: '2006-03-01'});
+var pg = require('pg');
+var _ = require("lodash");
+var fs = require("fs");
+// var dsn = "tcp://USER:PASSWORD@END_POINT:PORT/DBNAME";
+var dsn = "tcp://yalab@localhost:15432/redshift_test";
+
+var client = new pg.Client(dsn);
+client.connect();
+
+var rollback = function(client) {
+  client.query('ROLLBACK', function() {
+    client.end();
+  });
+};
 
 exports.handler = function(event, context) {
   var bucket = event.Records[0].s3.bucket.name;
@@ -12,15 +26,31 @@ exports.handler = function(event, context) {
       if(!body){
         break;
       }
-
-      logs.push(JSON.stringify(JSON.parse(body)["meteors"]));
+      var csv = _.values(JSON.parse(body)["meteors"]).join(",");
+      logs.push(csv);
     }
-    var s3bucket = new aws.S3({params: {Bucket: 'test-bucket'}});
-    s3bucket.upload({Key: "test-ext.log", Body: logs.join("\n")}, function(err, data) {
-      console.log(err);
-      });
 
-    console.log(logs.join("\n"));
+    var s3bucket = new aws.S3({params: {Bucket: 'test-bucket'}});
+    var fname = '/tmp/test.log';
+    fs.writeFile(fname, logs.join("\n") , function (err) {
+//    s3bucket.upload({Key: "test-ext.log", Body: logs.join("\n")}, function(err, data) {
+      var copy = "COPY meteors FROM '" + fname + "' WITH CSV";
+      console.log(copy);
+      client.query('BEGIN', function(err, result) {
+        if(err){
+          console.log("ERROR");
+          return rollback(client)
+        };
+        client.query(copy, function(err, result) {
+          if(err){
+            console.log("COPY ERROR");
+            console.log(err);
+            return rollback(client);
+          }
+          client.query("COMMIT;", client.end.bind(client));
+        });
+      });
+    });
    });
 };
 
