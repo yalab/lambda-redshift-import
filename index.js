c89f8b9881d54df096bf68c0a4c8538796e95740
yalab <rudeboyjet@gmail.com>
2016-01-06 19:34:25 +0900

    success to insert redshift without lambda.

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ var s3 = new aws.S3({apiVersion: '2006-03-01'});
 var pg = require('pg');
 var _ = require("lodash");
 var fs = require("fs");
-// var dsn = "tcp://USER:PASSWORD@END_POINT:PORT/DBNAME";
-var dsn = "tcp://yalab@localhost:15432/redshift_test";
+
+var bucketName = 'test-bucket';
+var destObjectName = "test-ext.log";
+var dsn = "tcp://USERNAME:PASSWORD@END_POINT:PORT/DBNAME";
+
+var awsAccessKeyId = "";
+var awsAccessSecret = "";
+var s3Region = "ap-northeast-1";
 
 var client = new pg.Client(dsn);
 client.connect();
+var tableName = "meteors";
 
 var rollback = function(client) {
   client.query('ROLLBACK', function() {
@@ -30,17 +37,16 @@ exports.handler = function(event, context) {
       logs.push(csv);
     }
 
-    var s3bucket = new aws.S3({params: {Bucket: 'test-bucket'}});
-    var fname = '/tmp/test.log';
-    fs.writeFile(fname, logs.join("\n") , function (err) {
-//    s3bucket.upload({Key: "test-ext.log", Body: logs.join("\n")}, function(err, data) {
-      var copy = "COPY meteors FROM '" + fname + "' WITH CSV";
-      console.log(copy);
+    var s3bucket = new aws.S3({params: {Bucket: bucketName}});
+
+    s3bucket.upload({Key: destObjectName, Body: logs.join("\n")}, function(err, data) {
+      var copy = "COPY " + tableName + " FROM 's3://" + bucketName + "/" + destObjectName + "' CREDENTIALS 'aws_access_key_id=" + awsAccessKeyId + ";aws_secret_access_key=" + awsAccessSecret + "' CSV;";
       client.query('BEGIN', function(err, result) {
         if(err){
           console.log("ERROR");
           return rollback(client)
         };
+        console.log(copy);
         client.query(copy, function(err, result) {
           if(err){
             console.log("COPY ERROR");
@@ -53,6 +59,3 @@ exports.handler = function(event, context) {
     });
    });
 };
-
-console.log(exports.handler({Records: [{s3: {bucket: {name: "test-bucket"},
-                                             object: {key: "test.log"}}}]}));
